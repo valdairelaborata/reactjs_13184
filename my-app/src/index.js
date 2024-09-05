defdf200a621da2629ac3a7029f94b4c1a0388de
Valdair <valdairelaborata@gmail.com>
2024-09-05 19:51:33 -0300

    element

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,16 +6,32 @@ import Opa from './Opa'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Opa />
-    <App />
-    <App />
-    <App />
-    <App />
-    
-  </React.StrictMode>
-);
+
+const nome = "Valdair"
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Olá, {nome}</h1>
+      <h2>Hora agora: {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+
+  root.render(element)
+}
+
+
+
+setInterval(tick, 1000);
+
+
+
+// root.render(
+//   <React.StrictMode>
+//     <Opa />
+//     <App />   
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
